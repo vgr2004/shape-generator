d0a3c390cf2b9d49f029a44d95d7be493321a338
vignesh gr <141708172+vgr2004@users.noreply.github.com>
2023-08-21 11:55:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+let button = document.getElementById("button");
+var numberInTheShape = 1;
+var oldValue = 0;
+
+button.onclick =() => {
+    let n = document.getElementById("number").value;
+    const circle = document.getElementById("circle");
+    const square = document.getElementById("square");
+    const rectangle = document.getElementById("rectangle");
+    const $box = document.getElementById("box");
+    let j;
+
+    n = Number(oldValue) + Number(n);
+
+    for( let j = numberInTheShape; j<=n; j++){
+        var shape = document.createElement("div");
+        shape.innerHTML += numberInTheShape;
+    if(square.checked){
+        shape.classList.add("square");
+    }
+    else if(circle.checked){
+         shape.classList.add("circle");
+    }
+    else if(rectangle.checked){
+          shape.classList.add("rectangle");
+    }
+    else{
+        document.write("invalid input");
+    }
+    $box.appendChild(shape);
+    numberInTheShape++;
+    oldValue = document.getElementById("box").lastElementChild.innerHTML;
+    
+}
+};
